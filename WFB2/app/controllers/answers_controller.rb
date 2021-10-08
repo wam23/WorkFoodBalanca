b6f0af43b4ed1f32760b3611ba071fdd7f9df462
hawkie23 <hawkie@bluewin.ch>
2021-10-08 18:44:11 +0200

    no message

--- WFB2/app/controllers/answers_controller.rb
+++ WFB2/app/controllers/answers_controller.rb
@@ -1,8 +1,17 @@
 class AnswersController < ApplicationController
+  
   def create
     @answer = Answer.new(answer_params)
     if @answer.save
-      redirect_to root_url
+      redirect_back(fallback_location: root_path)
+    else
+      render 'new'
+    end
+  end
+  
+  def answer_value
+    @answer = Answer.new(answer_params)
+    if @answer.save
       redirect_back(fallback_location: root_path)
     else
       render 'new'
