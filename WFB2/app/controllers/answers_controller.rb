9680b3d86aee6dc243f7702e63d1f67d0f823543
Rafael Wampfler <wampfler@gmail.com>
2021-10-08 18:08:33 +0200

    answer redirect

--- WFB2/app/controllers/answers_controller.rb
+++ WFB2/app/controllers/answers_controller.rb
@@ -1,18 +1,16 @@
 class AnswersController < ApplicationController
-
   def create
     @answer = Answer.new(answer_params)
     if @answer.save
-      redirect_to root_url
+      redirect_back(fallback_location: root_path)
     else
-      render 'new'
+      render "new"
     end
   end
 
   private
 
-    def answer_params
-      params.permit(:question_id, :user_id)    
-    end
-
+  def answer_params
+    params.permit(:question_id, :user_id)
+  end
 end
